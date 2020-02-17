595185d0c54e4b9efa20568c1940e33138ef87c8
hellocreep <hellocreep@gmail.com>
2020-02-17 14:44:57 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {}
+}
---
+++ src/.vuepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>layout</h1>
+    <Content />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
